0d9cbf172a1c5c210e4ff894fd5eb477e420964f
Irina <ten7473@mail.ru>
2021-06-09 18:53:37 +0500

    сделала нажатие цифр, операций, очистка всего и последнего символа

---
+++ Calculator/Calculator/InputString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class InputString
+    {
+        string TextBox_Input { get; set; }
+
+
+    }
+}
--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -25,9 +25,92 @@ namespace Calculator
             InitializeComponent();
         }
 
+        string InputString = "";
+
+        private void Button_0_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_0.Content;
+        }
+
         private void Button_1_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_1.Content;
         }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_2.Content;
+        }
+
+        private void Button_3_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_3.Content;
+        }
+
+        private void Button_4_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_4.Content;
+        }
+
+        private void Button_5_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_5.Content;
+        }
+
+        private void Button_6_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_6.Content;
+        }
+
+        private void Button_7_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_7.Content;
+        }
+
+        private void Button_8_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_8.Content;
+        }
+
+        private void Button_9_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_9.Content;
+        }
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text = "";
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text=TextBox_Input.Text.Remove(TextBox_Input.Text.Length-1);
+        }
+
+        private void Button_Plus_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_Plus.Content;
+        }
+
+        private void Button_Minus_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_Minus.Content;
+        }
+
+        private void Button_Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_Multiplication.Content;
+        }
+
+        private void Button_Division_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Input.Text += (string)Button_Division.Content;
+        }
+
+        private void Button_Result_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
