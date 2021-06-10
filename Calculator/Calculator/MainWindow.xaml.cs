814c23d6215252ed6c8061bc8063fa8bee58ae50
Irina <ten7473@mail.ru>
2021-06-10 08:22:51 +0500

    Примитивно работает с целыми числами. Падает при повторном равно. Нужно сделать множественные операции  и дробные числа.

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -25,61 +25,93 @@ namespace Calculator
             InitializeComponent();
         }
 
-        string InputString = "";
+        string InputNumber1 = " ";
+        string InputNumber2 = " ";
+        int number1, number2, result;
+        int operation;
+        
+        enum Operation {plus, minus, multiplication, division};
 
         private void Button_0_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_0.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_0.Content;
+            else InputNumber2 += (string)Button_0.Content;
+            //TextBlock_Result.Text = InputNumber1;
+            //TextBlock_Result.Text = InputNumber2;
         }
 
         private void Button_1_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_1.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_1.Content;
+            else InputNumber2 += (string)Button_1.Content;
+            //TextBlock_Result.Text = InputNumber1;
+            //TextBlock_Result.Text = InputNumber2;
         }
 
         private void Button_2_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_2.Content;
+            if (InputNumber1 !="") InputNumber1 += (string)Button_2.Content;
+            else InputNumber2 += (string)Button_2.Content;
+            //TextBlock_Result.Text = InputNumber1;
+            //TextBlock_Result.Text = InputNumber2;
         }
 
         private void Button_3_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_3.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_3.Content;
+            else InputNumber2 += (string)Button_3.Content;
         }
 
         private void Button_4_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_4.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_4.Content;
+            else InputNumber2 += (string)Button_4.Content;
         }
 
         private void Button_5_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_5.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_5.Content;
+            else InputNumber2 += (string)Button_5.Content;
         }
 
         private void Button_6_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_6.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_6.Content;
+            else InputNumber2 += (string)Button_6.Content;
         }
 
         private void Button_7_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_7.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_7.Content;
+            else InputNumber2 += (string)Button_7.Content;
         }
 
         private void Button_8_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_8.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_8.Content;
+            else InputNumber2 += (string)Button_8.Content;
         }
 
         private void Button_9_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_9.Content;
+            if (InputNumber1 != "") InputNumber1 += (string)Button_9.Content;
+            else InputNumber2 += (string)Button_9.Content;
         }
 
         private void Button_Clear_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text = "";
+            TextBlock_Result.Text = "";
         }
 
         private void Button_Delete_Click(object sender, RoutedEventArgs e)
@@ -90,25 +122,63 @@ namespace Calculator
         private void Button_Plus_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_Plus.Content;
+            number1 = Int32.Parse(InputNumber1);
+            InputNumber1 = "";
+            operation = 1;
+            
         }
 
         private void Button_Minus_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_Minus.Content;
+            number1 = Int32.Parse(InputNumber1);
+            InputNumber1 = "";
+            operation = 2;
         }
 
         private void Button_Multiplication_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_Multiplication.Content;
+            number1 = Int32.Parse(InputNumber1);
+            InputNumber1 = "";
+            operation = 3;
         }
 
         private void Button_Division_Click(object sender, RoutedEventArgs e)
         {
             TextBox_Input.Text += (string)Button_Division.Content;
+            number1 = Int32.Parse(InputNumber1);
+            InputNumber1 = "";
+            operation = 4;
         }
 
         private void Button_Result_Click(object sender, RoutedEventArgs e)
         {
+            
+            number2 = Int32.Parse(InputNumber2);
+            InputNumber2 = "";
+            switch (operation)
+            {
+                case 1: 
+                    result=number1 + number2;
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    break;
+                case 4:
+                    result = number1 / number2;
+                    break;
+                case 0:
+                    result = 0;
+                    break;
+            }
+            TextBlock_Result.Text = result.ToString();
+            operation = 0;
+            InputNumber1 = "0";
+
 
         }
 
